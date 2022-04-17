dfd8374ac239b4fcad852b3932476e92b0178f7f
zahra <zsabah559@gmail.com>
2022-04-17 18:44:36 +0430

    Add click handler in Cross component

--- src/components/Cross/Cross.tsx
+++ src/components/Cross/Cross.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./styles.scss";
 
-const Cross = (props: { className?: string }) => {
-  return <span className={`${props.className} cross `} />;
+const Cross = (props: { className?: string; onClick: () => void }) => {
+  return (
+    <span className={`${props.className} cross`} onClick={props.onClick} />
+  );
 };
 
 export default Cross;
