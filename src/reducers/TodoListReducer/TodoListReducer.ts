7233d2da87147f889932b2304c9e477c44eccf0e
zahra <zsabah559@gmail.com>
2022-04-17 14:18:18 +0430

    Add a reducer in order to add a task to lest

--- src/reducers/TodoListReducer/TodoListReducer.ts
+++ src/reducers/TodoListReducer/TodoListReducer.ts
@@ -1,11 +1,23 @@
 import { TodoListStates } from "../../providers/TodoListProvider/TodoListProvider";
 import { ReducerActionType } from "../../utils/types";
 
+export const ADD_TASK = "ADD_TASK";
+
 export const TodoListReducer = (
   state: TodoListStates,
   action: ReducerActionType
 ) => {
   switch (action.type) {
+    case ADD_TASK: {
+      const newList = [
+        ...state.list,
+        { text: action.payload.text, isCompleted: false },
+      ];
+      return {
+        ...state,
+        list: newList,
+      };
+    }
     default:
       return state;
   }
