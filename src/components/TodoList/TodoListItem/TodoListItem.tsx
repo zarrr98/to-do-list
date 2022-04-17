8ddcb7b1d16ab759056dcac325ac63bdcd23f47d
zahra <zsabah559@gmail.com>
2022-04-17 19:14:38 +0430

    Add a remove animation to tasks when they get deleted

--- src/components/TodoList/TodoListItem/TodoListItem.tsx
+++ src/components/TodoList/TodoListItem/TodoListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoListItemType } from "../../../utils/types";
 import Cross from "../../Cross/Cross";
 import "./styles.scss";
@@ -10,10 +10,32 @@ interface Props {
 }
 
 const TodoListItem = ({ item, style, deleteItem }: Props) => {
+  const [isDeleted, setIsDeleted] = useState(false);
+  const handleDeletingItems = () => {
+    setIsDeleted(true);
+    setTimeout(() => {
+      deleteItem();
+    }, 500);
+  };
+
   return (
-    <div className="todo-list-item" style={style}>
-      <p>{item.text}</p>
-      <Cross className="todo-list-item__delete" onClick={deleteItem} />
+    <div
+      className={`todo-list-item ${isDeleted && "todo-list-item--zero-height"}`}
+      style={style}
+    >
+      <p
+        className={`todo-list-item__text ${
+          isDeleted && "todo-list-item__text--deleted"
+        }`}
+      >
+        {item.text}
+      </p>
+      <Cross
+        className={`todo-list-item__delete ${
+          isDeleted && "todo-list-item__delete--deleted"
+        }`}
+        onClick={handleDeletingItems}
+      />
     </div>
   );
 };
