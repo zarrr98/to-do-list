0d70dc82016eaf6d875144ae8a473b9df7041574
zahra <zsabah559@gmail.com>
2022-04-17 13:26:14 +0430

    Wrap the whole to do list content by the provider

--- src/pages/TodoList/TodoList.tsx
+++ src/pages/TodoList/TodoList.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import TodoListContent from "../../components/TodoList/TodoListContent/TodoListContent";
+import TodoListProvider from "../../providers/TodoListProvider/TodoListProvider";
 
 const TodoList = () => {
-  return <div>list goes here...</div>;
+  return (
+    <TodoListProvider>
+      <TodoListContent />
+    </TodoListProvider>
+  );
 };
 
 export default TodoList;
