b5b0ba548f8a0544e3fdd5f98efe49ddef407317
zahra <zsabah559@gmail.com>
2022-04-18 22:05:49 +0430

    Add functions to get tasks from storage and also check their validity

--- src/components/TodoList/functions.ts
+++ src/components/TodoList/functions.ts
@@ -5,3 +5,24 @@ const TODO_LIST_KEY = "todo-list-tasks";
 export const storeTasksInStorage = (tasks: TodoListItemType[]) => {
   localStorage.setItem(TODO_LIST_KEY, JSON.stringify(tasks));
 };
+
+export const getTasksFromStorage = () => {
+  let tasks = localStorage.getItem(TODO_LIST_KEY);
+  if (!tasks) return [];
+  tasks = JSON.parse(tasks);
+  const isValid = isTodoTasksValid(tasks);
+  return isValid ? tasks : [];
+};
+
+const isTodoTasksValid = (tasks: any) => {
+  const essentialKeys = ["id", "text", "isCompleted"];
+
+  if (!tasks || !Array.isArray(tasks)) return false;
+  for (const task of tasks) {
+    for (const key of essentialKeys) {
+      if (!(key in task)) return false;
+    }
+  }
+
+  return true;
+};
