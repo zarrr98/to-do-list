3987093f86319206dfa05544bb97609be3eedf29
zahra <zsabah559@gmail.com>
2022-04-18 17:46:44 +0430

    Remove all completed tasks by clicking on the corresponding button

--- src/components/TodoList/TodoListFooter/TodoListFooter.tsx
+++ src/components/TodoList/TodoListFooter/TodoListFooter.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { TodoListContext } from "../../../providers/TodoListProvider/TodoListProvider";
+import Button from "../../Button/Button";
 import TodoListFilter from "../TodoListFilter/TodoListFilter";
 import "./styles.scss";
 
@@ -13,12 +14,26 @@ const TodoListFooter = () => {
     }, 0);
   };
 
+  const removeCompletedTasks = () => {
+    const completes = getCompletedTaskIds();
+    context.removeTaskFromList(completes);
+  };
+
+  const getCompletedTaskIds = () => {
+    return context.list.reduce((prev, current) => {
+      let tasks = [...prev];
+      if (current.isCompleted) tasks.push(current.id);
+      return tasks;
+    }, [] as string[]);
+  };
+
   return context.list.length ? (
     <div className="todo-list-footer">
       <p>
         <strong>{getNumberofUndoneTasks()}</strong> items left
       </p>
       <TodoListFilter />
+      <Button title="Clear completed" onClick={removeCompletedTasks} />
     </div>
   ) : (
     <></>
