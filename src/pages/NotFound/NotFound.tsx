daae7b58c522da115e74027c02a9633513b7d548
zahra <zsabah559@gmail.com>
2022-04-19 11:51:36 +0430

    Improve not-found page content

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -8,11 +8,11 @@ const NotFound = () => {
       <p className="not-found__code">404</p>
       <h1 className="not-found__title">Lost your way?</h1>
       <p className="not-found__desc">
-        Sorry, we can't find that page. Don't worry though, you can get back to
-        the home page.
+        Sorry, we can't find that page. Don't worry though, you can get back and
+        keep managing your tasks.
       </p>
       <Link to={"/"} className="not-found__btn">
-        Home Page
+        To-Do List
       </Link>
     </div>
   );
